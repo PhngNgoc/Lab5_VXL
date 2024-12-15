068ab749b5265c0d406dd1a0d13fd14ddf406413
PhngNgoc <159431247+PhngNgoc@users.noreply.github.com>
2024-12-15 23:58:31 +0700

    add stm32

---
+++ Lab5/Core/Inc/sofware_timer.h
@@ -0,0 +1,22 @@
+/*
+ * sofware_timer.h
+ *
+ *  Created on: Oct 28, 2024
+ *      Author: tranm
+ */
+
+#ifndef INC_SOFWARE_TIMER_H_
+#define INC_SOFWARE_TIMER_H_
+
+
+
+extern int timer1_flag;
+
+
+
+void setTimer1(int duration);
+
+void timerRun();
+
+
+#endif /* INC_SOFWARE_TIMER_H_ */
